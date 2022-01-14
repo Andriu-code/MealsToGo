f73a6ef5ec9cb525e70de1026fe041c23270b2d8
Andrea Leal <a.lealhernandez91@gmail.com>
2022-01-14 14:07:31 -0600

    master

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -23,7 +23,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     name = "Some Restaurant",
     icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
-      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+      "https://laroussecocina.mx/wp-content/uploads/2019/07/S050419-14-TACOS-0459.jpg",
     ],
     address = "100 some random street",
     isOpenNow = true,
